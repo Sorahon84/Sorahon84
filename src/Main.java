36a43f11b091d3dfcb647451d184c33bcfbd8e57
Sora <shohkuch@gmail.com>
2024-08-12 18:46:05 -0500

    name

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
-
-
+        System.out.println("hello Syntax!");
+        System.out.println("hello Batch20");
     }
 }
